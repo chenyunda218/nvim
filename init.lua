b21a0fa2ad47a4d6b7704a0db0b755ae660ec938
陈允达 <dan@datastory.com.cn>
2023-07-13 23:38:47 +0800

    push

---
+++ init.lua
@@ -0,0 +1,39 @@
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.opt.shiftwidth = 2
+vim.o.relativenumber = true
+
+-- Leader key
+vim.g.mapleader = ";"
+-- Mapping
+-- Map key function
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap=true, silent=true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+map('n', '<leader>n', ':NvimTreeToggle<CR>')
+map('n', '<leader>f', ':NvimTreeFocus<CR>')
+-- Lazy vim configuration
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({import = "plugins"})
+
+-- BufferLine
+map('n', '<leader>p', ':BufferLinePick<CR>')
+vim.cmd.colorscheme "catppuccin"
+
