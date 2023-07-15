ae1e62eb2af2f772be53b231cb01f113b5cf1518
陈允达 <dan@datastory.com.cn>
2023-07-15 09:17:33 +0800

    fixup

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -11,5 +11,23 @@ return {
         } }
       }
     }
+    vim.api.nvim_set_keymap(
+      'n',
+      '<leader>c',
+      ':BufferLineCloseOthers<CR>',
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      'n',
+      '<leader>x',
+      ':BufferLinePickClose<CR>',
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      'n',
+      '<leader>z',
+      ':BufferLinePickClose<CR>',
+      { noremap = true, silent = true }
+    )
   end,
 }
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require "lspconfig"
+      vim.api.nvim_set_keymap('n', '<leader>d', ':lua vim.lsp.buf.definition()<CR>', {
+        noremap = true,
+        silent = true,
+      })
+      vim.api.nvim_set_keymap('n', '<leader>l', ':lua vim.lsp.buf.format()<CR>',
+      {
+        noremap = true,
+        silent = true,
+      })
+      -- python
+      lspconfig.pyright.setup {}
+      -- typescript
+      lspconfig.tsserver.setup {}
+      -- java
+      -- lspconfig.jdtls.setup {}
+      -- lua
+      lspconfig.lua_ls.setup {}
+      -- golang
+      lspconfig.gopls.setup {}
+    end,
+  }
+}
