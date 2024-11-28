03cd15ba59d14e1697acd9ac12889bdad440c4e3
ChenYunDa <chenyunda218@gmail.com>
2024-11-28 20:41:54 +0800

    first

---
+++ init.lua
@@ -0,0 +1,26 @@
+vim.opt.termguicolors = true
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.opt.shiftwidth = 2
+vim.o.relativenumber = true
+
+require("config.lazy")
+
+function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+
+map('n', '<leader>n', ':NvimTreeToggle<CR>')
+map('t', '<C-Esc>', '<C-\\><C-n>')
+map('n', '<leader>ff', ':Telescope find_files<CR>')
+map('n', '<leader>fg', ':Telescope live_grep<CR>')
+map('n', '<leader>fb', ':Telescope buffers<CR>')
+map('n', '<leader>fh', ':Telescope help_tags<CR>')
+map('n', '<leader>w', '<C-w>')
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate", 
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = {
+        "typescript",
+        "javascript", 
+        "go", "python", "lua", "vim", "json",
+      },
+      highlight = {
+        enable = true, -- false will disable the whole extension
+      },
+    }
+  end,
+}
