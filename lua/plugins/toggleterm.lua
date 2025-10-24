02a1234090d2661e29bd851ae87c824aaddf37c0
ChenYunDa <chenyunda218@gmail.com>
2025-10-25 07:38:43 +0800

    feat: save buffer

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {--[[ things you want to change go here]]
+		open_mapping = [[<c-\>]],
+		insert_mappings = true,
+		close_on_exit = true,
+		direction = "horizontal",
+	},
+	config = true,
+}
