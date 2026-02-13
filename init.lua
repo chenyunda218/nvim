9b1507b7adaa33ef1ad9f17de24f55e102ccc58c
ydc3148 <ydc3148@dsscu.gov.mo>
2026-02-13 18:07:28 +0800

    fixup

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@ vim.pack.add({
   {src = "https://github.com/nvim-tree/nvim-tree.lua"},
   {src = "https://github.com/karb94/neoscroll.nvim"},
   {src = "https://github.com/catppuccin/nvim"},
+  {src = "https://github.com/akinsho/bufferline.nvim"},
+  {src = "https://github.com/nvim-tree/nvim-web-devicons"},
+  {src = "https://github.com/petertriho/nvim-scrollbar"},
 })
 
 vim.opt.termguicolors = true
@@ -17,8 +20,17 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", { desc = "toggle nvim tree" })
 
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<leader>p", "<cmd>BufferLinePick<CR>")
+
 require("mason").setup()
 require("plugins/lualine")
 require("plugins/nvim-tree")
 require("plugins/neoscroll")
 require("plugins/theme")
+require("plugins/bufferline")
+require("plugins/nvim-web-devicons")
+require("plugins/nvim-scrollbar")
