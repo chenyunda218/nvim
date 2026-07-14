34c000a0239e7ba221b3906c3199d21fd344eee5
ydc3148 <ydc3148@dsscu.gov.mo>
2026-07-14 11:36:58 +0800

    fixup

--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
 require("config.lazy")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.mapleader = " "
---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,7 @@
+return {
+  "nickjvandyke/opencode.nvim",
+  version = "*", -- Latest stable release
+  config = function()
+    ---@type opencode.Opts
+  end,
+}
