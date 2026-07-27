a1660f1193961c3470bd2c86859f91ecf3ad2e3b
ydc3148 <ydc3148@dsscu.gov.mo>
2026-07-27 09:08:26 +0800

    fixup

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@ require("config.lazy")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
+vim.keymap.set("i", "<ESC>", "<cmd>nohlsearch<CR>")
---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").setup({
+			-- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
+	end,
+}
